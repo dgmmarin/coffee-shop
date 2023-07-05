46df3ff2a494f7275ada8c3ad1144a727f09fa7b
Daniel Marin <marin.daniel.g@gmail.com>
2023-07-05 08:49:30 +0300

    add files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './components/product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmAsyncConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    ProductModule, 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,23 @@
+import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+    useFactory: async():Promise<TypeOrmModuleOptions> => {
+        return {
+            type: 'mysql',
+            host: '127.0.0.1',
+            port: 3306,
+            username: 'root',
+            password: 'Qwerty123456!@#$%^',
+            database: 'coffee_shop',
+            entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+            migrations: [__dirname + '/../database/migrations/*{.ts/.js}'],
+            migrationsTableName: 'migrations',
+            logging: true,
+            synchronize:true,
+            migrationsRun: true,
+            extra:{
+                "insecureAuth": true    
+            }
+        };
+    }
+}
