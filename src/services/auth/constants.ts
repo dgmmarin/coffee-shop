9a9d5ac6a9fb5ebca6aacfe0138cfdd62bcd203d
Daniel Marin <marin.daniel.g@gmail.com>
2023-07-10 10:27:32 +0300

    add is public for routes

--- src/services/auth/constants.ts
+++ src/services/auth/constants.ts
@@ -1,4 +1,5 @@
 import { config } from 'dotenv';
+import { SetMetadata } from '@nestjs/common';
 
 config();
 const { BIT_NFT_API_SECRET } = process.env;
@@ -6,3 +7,7 @@ const { BIT_NFT_API_SECRET } = process.env;
 export const jwtConstants = {
   secret: BIT_NFT_API_SECRET,
 };
+
+
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
