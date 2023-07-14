840575bc50e4310fa8b349422114cd7f2a9fc0f4
Daniel Marin <marin.daniel.g@gmail.com>
2023-07-14 16:02:03 +0300

    add data on order

--- src/components/order/order.service.ts
+++ src/components/order/order.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class OrderService {
+  constructor(
+    @InjectRepository(Order) private orderRepository: Repository<Order>
+  ){
+
+  }
   create(createOrderDto: CreateOrderDto) {
     return 'This action adds a new order';
   }
